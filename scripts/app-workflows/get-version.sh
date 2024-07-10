4d2bd27e59ac6eb9cc441285dea03b733cccbafd
E. Lynette Rayle <elrayle@github.com>
2024-07-09 21:46:41 -0400

    pass in root dir

--- scripts/app-workflows/get-version.sh
+++ scripts/app-workflows/get-version.sh
@@ -5,12 +5,12 @@
 #   $2 - is_release: true if the deployment is triggered by a release, false otherwise
 #   $3 - release_tag: the tag of the release that triggered the deployment (empty if not a release)
 #   $4 - sha: the git sha of the commit being deployed
-#   $5 - lock_file: the path to the package-lock.json file (default: /package-lock.json)
+#   $5 - rootdir: the path where package-lock.json file lives
 #
 # Outputs
 #   version: the version of the package to deploy (e.g. v1.2.0, v1.2.0-dev-1234567890)
 
-echo "*** Entering get-version.sh -> inputs -> deploy_env: $1, is_release: $2, release_tag: $3, sha: $4, lock_file: $5"
+echo "*** Entering get-version.sh -> inputs -> deploy_env: $1, is_release: $2, release_tag: $3, sha: $4, rootdir: $5"
 
 # ROOT_DIR=$(git rev-parse --show-toplevel)
 # echo "ROOT_DIR: $ROOT_DIR"
@@ -19,13 +19,14 @@ echo "*** Entering get-version.sh -> inputs -> deploy_env: $1, is_release: $2, r
 # is_release="$2"
 # release_tag="$3"
 # sha="$4"
-# lock_file="${5:-$ROOT_DIR/package-lock.json}"
+# rootdir="$5"
 
 # echo "deploy_env: $deploy_env"
 # echo "is_release: $is_release"
 # echo "release_tag: $release_tag"
 # echo "sha: $sha"
-# echo "lock_file: $lock_file"
+# echo "rootdir: $rootdir"
+# echo lock_file="$5/package-lock.json"
 
 # version='v'$(jq -r '.version' $lock_file) # e.g. v1.2.0
 # echo "version: $version"
