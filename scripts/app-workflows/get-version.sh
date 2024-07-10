7f468ce582ee64b0b72e30af7680fc57fe187eb8
E. Lynette Rayle <elrayle@github.com>
2024-07-09 21:36:43 -0400

    debug

--- scripts/app-workflows/get-version.sh
+++ scripts/app-workflows/get-version.sh
@@ -12,39 +12,39 @@
 
 echo "*** Entering get-version.sh -> inputs -> deploy_env: $1, is_release: $2, release_tag: $3, sha: $4, lock_file: $5"
 
-ROOT_DIR=$(git rev-parse --show-toplevel)
-echo "ROOT_DIR: $ROOT_DIR"
+# ROOT_DIR=$(git rev-parse --show-toplevel)
+# echo "ROOT_DIR: $ROOT_DIR"
 
-deploy_env="$1"
-is_release="$2"
-release_tag="$3"
-sha="$4"
-lock_file="${5:-$ROOT_DIR/package-lock.json}"
+# deploy_env="$1"
+# is_release="$2"
+# release_tag="$3"
+# sha="$4"
+# lock_file="${5:-$ROOT_DIR/package-lock.json}"
 
-echo "deploy_env: $deploy_env"
-echo "is_release: $is_release"
-echo "release_tag: $release_tag"
-echo "sha: $sha"
-echo "lock_file: $lock_file"
+# echo "deploy_env: $deploy_env"
+# echo "is_release: $is_release"
+# echo "release_tag: $release_tag"
+# echo "sha: $sha"
+# echo "lock_file: $lock_file"
 
-version='v'$(jq -r '.version' $lock_file) # e.g. v1.2.0
-echo "version: $version"
+# version='v'$(jq -r '.version' $lock_file) # e.g. v1.2.0
+# echo "version: $version"
 
-if [[ "$deploy_env" == 'prod' ]]; then
-  if [[ "$is_release" == 'true' ]]; then
-    # validate the version when triggered by a release
-    if [[ "$version" != "$release_tag" ]]; then
-      echo "Version in package-lock.json ($version) does not match the release tag ($release_tag)"
-      exit 1
-    fi
-  fi
-elif [[ "$deploy_env" == 'dev' ]]; then
-  short_sha=$(echo "$sha" | cut -c 1-10)
-  version=$version'-dev-'$short_sha       # e.g. v1.2.0-dev-1234567890
-else
-  echo "Invalid deploy environment: $deploy_env. Must be 'dev' or 'prod'"
-  exit 1
-fi
+# if [[ "$deploy_env" == 'prod' ]]; then
+#   if [[ "$is_release" == 'true' ]]; then
+#     # validate the version when triggered by a release
+#     if [[ "$version" != "$release_tag" ]]; then
+#       echo "Version in package-lock.json ($version) does not match the release tag ($release_tag)"
+#       exit 1
+#     fi
+#   fi
+# elif [[ "$deploy_env" == 'dev' ]]; then
+#   short_sha=$(echo "$sha" | cut -c 1-10)
+#   version=$version'-dev-'$short_sha       # e.g. v1.2.0-dev-1234567890
+# else
+#   echo "Invalid deploy environment: $deploy_env. Must be 'dev' or 'prod'"
+#   exit 1
+# fi
 
-echo "get-version.sh -> outputs -> version: $version"
-echo "$version"
+# echo "get-version.sh -> outputs -> version: $version"
+# echo "$version"
